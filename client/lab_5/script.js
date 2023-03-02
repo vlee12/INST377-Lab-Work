61bdf62c7de54940dbe5cd0f17f0c237c7438bf1
vlee12 <vlee12@terpmail.umd.edu>
2023-03-01 23:37:13 -0500

    Updated Lab 5

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,6 +5,12 @@
 
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  })
+  
   /*
     Using the .filter array method, 
     return a list that is filtered by comparing the item name in lower case
@@ -15,7 +21,9 @@ function filterList(list, query) {
 
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  
   // Add a querySelector that targets your filter button here
+  const filterButton = document.querySelector('.filter_button');
 
   let currentList = []; // this is "scoped" to the main event function
   
@@ -52,6 +60,18 @@ async function mainEvent() { // the async keyword means we can make API requests
     */
     console.table(currentList); 
   });
+
+  filterButton.addEventListener('click', (event) => {
+    console.log('clicked FilterButton');
+
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps)
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList);
+  })
 
 
   /*
